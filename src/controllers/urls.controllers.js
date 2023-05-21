98519b88a568ccb339a4046f3bb4419eb9c10e02
lbritors <britoleticiars@gmail.com>
2023-05-21 17:09:24 -0300

    fix: controller inserting urls

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -12,12 +12,14 @@ export async function createURL(req, res) {
         const session = await checkToken(token);
         if (!session.rowCount) return res.sendStatus(401); 
         const insertUrl = await insertUrlDB(url, session.rows[0].user_id, urlShortened);
-        const getIdUrl = await getUrlDB(urlShortened);
-        const object = {
-            id: getIdUrl.rows[0].url_id,
-            shortUrl: getIdUrl.rows[0].short_url
-        }
-        res.status(201).send(object);
+        console.log(insertUrl.rows[0]);
+        // const getIdUrl = await getUrlDB(urlShortened);
+        // const object = {
+        //     id: getIdUrl.rows[0].url_id,
+        //     shortUrl: getIdUrl.rows[0].short_url
+        // }
+        // res.status(201).send(object);
+        res.status(201).send(insertUrl.rows[0]);
     } catch (err) {
         res.status(500).send(err.message);
     } 
